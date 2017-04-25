2c7105cd5e2bc94a37c1b6b25ed90650b2e6195c
ALEX SIMEON <ALEX SIMEON>
2017-04-25 21:50:13 +0100

    carguruji

---
+++ src/test/java/StepDef/testRunner.java
@@ -0,0 +1,15 @@
+package StepDef;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		format = {"pretty", "html:target/html/"},
+		features = "src/test/resource"
+		)
+public class testRunner {
+
+}
